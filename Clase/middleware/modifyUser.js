eb69a99223cd801b724e65b06e5e5d41183f49fe
Lara Hershko <sarayhershko@gmail.com>
2025-03-06 10:42:28 +0100

    Update modifyUser.js

--- Clase/middleware/modifyUser.js
+++ Clase/middleware/modifyUser.js
@@ -1,6 +1,6 @@
 const handleHttpError = require('../utils/handleHttpError');
 
-//lo que queremos es, que si eres admin, puedes 
+//TODO :lo que queremos es, que si eres admin, puedes editar lo que quieras, pero si eres user, modificar tus campos menos tu rol
 const checkRoleToModifyUser = (role)  => (req, res, next)=>{
     try {
         
